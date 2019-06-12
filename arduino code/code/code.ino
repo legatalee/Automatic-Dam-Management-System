7b2b7b94dc443bb7ff628b2394f8cde5233b2e47
legatalee <legatalee@gmail.com>
2019-06-12 15:26:03 +0900

    edited

--- arduino code/code/code.ino
+++ arduino code/code/code.ino
@@ -4,7 +4,7 @@
 #include <ESP8266WebServer.h>
 #include <ESP8266HTTPClient.h>
 #include <FirebaseArduino.h>
-#include <SoftwareSerial.h>
+//#include <SoftwareSerial.h>
 
 #define FIREBASE_HOST "automatic-dam.firebaseio.com"
 #define FIREBASE_AUTH "3rxeL5tbMRMQn7qecowaJumq9XDd7MRH1DS6Hd3l"
@@ -14,15 +14,17 @@
 //#define WIFI_PASSWORD "dimidimi"
 
 //SoftwareSerial HC12(D11, D12);
-SoftwareSerial in(D4, D3);
+//SoftwareSerial in(D4, D3);
 
 HTTPClient http;
 //WiFiClient client;
 
 void setup() {
-  pinMode(D1, OUTPUT);
+  pinMode(D7, OUTPUT);
+  pinMode(D6, OUTPUT);
   pinMode(D2, OUTPUT);
-  pinMode(2, OUTPUT);
+  pinMode(D1, INPUT);
+
   Serial.begin(115200);
   //  HC12.begin(9600);
 
@@ -38,23 +40,19 @@ void setup() {
 
   Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
   Firebase.stream("/queue");
-
-  //  client.connect("49.247.130.104", 1003);
 }
 
 String path;
 String data;
 int cnt = 0;
 
+unsigned long prev_time = 0;
+float duration, level;
+String echo = "";
+
 void loop() {
   String path = "";
   String data = "";
-  String sensor = "";
-
-  while (in.available()) {
-    sensor += (char) in.read();
-    delay(1);
-  }
 
   if (Firebase.available()) {
     FirebaseObject event = Firebase.readEvent();
@@ -71,45 +69,53 @@ void loop() {
   }
 
   if (data == "on1") {
-    digitalWrite(D1, HIGH);
+    digitalWrite(D7, HIGH);
     removeData(path);
   }
   if (data == "off1") {
-    digitalWrite(D1, LOW);
+    digitalWrite(D7, LOW);
     removeData(path);
   }
   if (data == "on2") {
-    digitalWrite(D2, HIGH);
+    digitalWrite(D6, HIGH);
     removeData(path);
   }
   if (data == "off2") {
-    digitalWrite(D2, LOW);
+    digitalWrite(D6, LOW);
     removeData(path);
   }
   if (data == "onall") {
-    digitalWrite(D1, HIGH);
-    digitalWrite(D2, HIGH);
+    digitalWrite(D7, HIGH);
+    digitalWrite(D6, HIGH);
     removeData(path);
   }
   if (data == "offall") {
-    digitalWrite(D1, LOW);
-    digitalWrite(D2, LOW);
+    digitalWrite(D7, LOW);
+    digitalWrite(D6, LOW);
     removeData(path);
   }
-  Serial.println(sensor);
-  String postData = "data=" + sensor;
-  Serial.println("get in");
-  HTTPClient http;
-  http.begin("http://172.16.2.31/stat/data");
-  //  http.begin("http://49.247.130.104/stat/data");
-  http.addHeader("Content-Type", "application/x-www-form-urlencoded");
-  int httpResponseCode = http.POST(postData);
-  http.POST(postData);
-  http.end();
-  //  client.println(postData);
 
-  cnt++;
-  Serial.println(cnt);
+  digitalWrite(D2, HIGH);
+  unsigned long current_time = millis();
+  if (current_time - prev_time > 15) {
+    digitalWrite(D2, LOW);
+    duration = pulseIn(D1, HIGH);
+    level = map(duration, 970, 50, 0, 180);
+    if (level < 0)level = 0; echo = String(level / 10);
+    echo = echo.substring(0, echo.length() - 1);
+
+    String postData = "data=" + String(analogRead(A0) * (5.0 / 1024.0)) + ',' + echo;
+    Serial.println(postData);
+
+    HTTPClient http;
+    http.begin("http://49.247.130.104/stat/data");
+    http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+    int httpResponseCode = http.POST(postData);
+    http.POST(postData);
+    http.end();
+
+    prev_time = millis();
+  }
 }
 
 void removeData(String path) {
